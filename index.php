0a6e412a8cab4de6dc8a7ea3cf1ef6e1ebb2b1fe
marcantonio99 <marcantonio8799@gamil.com>
2022-12-12 17:22:30 +0100

    I print the paragraph and its length

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo( 'ciao' );
+
+$paragraph = 
+'Astro del ciel,
+pargol divin,
+mite Agnello redentor,
+tu che i vati da lungi sognar,
+tu che angeliche voci annunziar
+luce dona alle menti,
+pace infondi nei cuor.';
+
+var_dump( strlen($paragraph) );
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <p><?php echo $paragraph ?></p>
+</body>
+</html>
